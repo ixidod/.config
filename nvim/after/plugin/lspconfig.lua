8acfed7e364795e681a8eb7fb3aa8318f5a573ec
Patrycjusz Myszka <pat.myszka@gmail.com>
2023-07-08 15:24:57 +0100

    lsp

--- nvim/after/plugin/lspconfig.lua
+++ nvim/after/plugin/lspconfig.lua
@@ -23,17 +23,3 @@ lsp.format_on_save({
 	},
 })
 lsp.setup()
-
-local null_ls = require("null-ls")
-local null_opts = lsp.build_options("null-ls", {})
-
-null_ls.setup({
-	on_attach = function(client, bufnr)
-		null_opts.on_attach(client, bufnr)
-	end,
-	sources = {
-		null_ls.builtins.formatting.stylua,
-		null_ls.builtins.formatting.prettier,
-		null_ls.builtins.formatting.clang_format,
-	},
-})
