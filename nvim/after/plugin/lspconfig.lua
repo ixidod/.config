0031359faad719c197518a72d4691b40d219c5d2
Patrycjusz Myszka <pat.myszka@gmail.com>
2023-12-26 21:27:24 +0000

    cmp

--- nvim/after/plugin/lspconfig.lua
+++ nvim/after/plugin/lspconfig.lua
@@ -2,10 +2,7 @@ local lsp = require("lsp-zero")
 
 lsp.preset("recommended")
 
-lsp.ensure_installed({
-	"tsserver",
-	"rust_analyzer",
-})
+
 
 -- Fix Undefined global 'vim'
 lsp.nvim_workspace()
@@ -13,14 +10,13 @@ lsp.nvim_workspace()
 local cmp = require("cmp")
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
-	["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-	["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-	["<C-y>"] = cmp.mapping.confirm({ select = true }),
+	["<C-k>"] = cmp.mapping.select_prev_item(cmp_select),
+	["<C-j>"] = cmp.mapping.select_next_item(cmp_select),
 	["<C-Space>"] = cmp.mapping.complete(),
 })
 
-cmp_mappings["<Tab>"] = nil
-cmp_mappings["<S-Tab>"] = nil
+--cmp_mappings["<Tab>"] = nil
+--cmp_mappings["<S-Tab>"] = nil
 
 lsp.setup_nvim_cmp({
 	mapping = cmp_mappings,
