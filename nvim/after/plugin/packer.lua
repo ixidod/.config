714bac2b027949e8bae0c3a9596a137eba4e59d7
Patrycjusz Myszka <pat.myszka@gmail.com>
2023-12-29 18:32:15 +0000

    null fucking ls

--- nvim/after/plugin/packer.lua
+++ nvim/after/plugin/packer.lua
@@ -1,41 +1,41 @@
 vim.cmd([[packadd packer.nvim]])
 
 return require("packer").startup(function(use)
-	use("wbthomason/packer.nvim")
+  use("wbthomason/packer.nvim")
 
- 	use("nvim-treesitter/nvim-treesitter")
+  use("nvim-treesitter/nvim-treesitter")
   use("fatih/vim-go")
-	use("windwp/nvim-autopairs")
+  use("windwp/nvim-autopairs")
+ use('jose-elias-alvarez/null-ls.nvim')
 
+  use({ "rose-pine/neovim", as = "rose-pine" })
 
-	use({ "rose-pine/neovim", as = "rose-pine" })
+  use({
+    "VonHeikemen/lsp-zero.nvim",
+    branch = "v1.x",
+    requires = {
+      -- LSP Support
+      { "neovim/nvim-lspconfig" }, -- Required
+      { "williamboman/mason.nvim" }, -- Optional
+      { "williamboman/mason-lspconfig.nvim" }, -- Optional
 
-	use({
-		"VonHeikemen/lsp-zero.nvim",
-		branch = "v1.x",
-		requires = {
-			-- LSP Support
-			{ "neovim/nvim-lspconfig" }, -- Required
-			{ "williamboman/mason.nvim" }, -- Optional
-			{ "williamboman/mason-lspconfig.nvim" }, -- Optional
+      -- Autocompletion
+      { "hrsh7th/nvim-cmp" }, -- Required
+      { "hrsh7th/cmp-nvim-lsp" }, -- Required
+      { "hrsh7th/cmp-buffer" }, -- Optional
+      { "hrsh7th/cmp-path" }, -- Optional
 
-			-- Autocompletion
-			{ "hrsh7th/nvim-cmp" }, -- Required
-			{ "hrsh7th/cmp-nvim-lsp" }, -- Required
-			{ "hrsh7th/cmp-buffer" }, -- Optional
-			{ "hrsh7th/cmp-path" }, -- Optional
+      -- Snippets
+      { "L3MON4D3/LuaSnip" }, -- Required
+      { "rafamadriz/friendly-snippets" }, -- Optional
+      { "mlaursen/vim-react-snippets" },
+      { "SirVer/ultisnips" },
+    },
+  })
 
-			-- Snippets
-			{ "L3MON4D3/LuaSnip" }, -- Required
-			{ "rafamadriz/friendly-snippets" }, -- Optional
-			{ "mlaursen/vim-react-snippets" },
-			{ "SirVer/ultisnips" },
-		},
-	})
-
-	use({
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.4",
-		requires = { { "nvim-lua/plenary.nvim" } },
-	})
+  use({
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.4",
+    requires = { { "nvim-lua/plenary.nvim" } },
+  })
 end)
