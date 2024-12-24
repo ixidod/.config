64c57bb66789404257cc27023abed0f82d21d49e
Patrycjusz Myszka <pat.myszka@gmail.com>
2024-12-24 01:02:57 +0000

    cc

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -2,9 +2,7 @@ local lsp = require("lsp-zero")
 
 lsp.preset("recommended")
 
-lsp.ensure_installed({
-	"tsserver",
-})
+lsp.ensure_installed({})
 
 -- Fix Undefined global 'vim'
 lsp.nvim_workspace()
