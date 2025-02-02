65e20aa65928e87795be329ddf7180989f7db90d
Patrycjusz Myszka <pat.myszka@gmail.com>
2025-02-02 14:53:04 +0000

    yaml fh

--- nvim/after/plugin/yaml-lsp.lua
+++ nvim/after/plugin/yaml-lsp.lua
@@ -1,17 +1,24 @@
 local capabilities = vim.lsp.protocol.make_client_capabilities()
+local cmp_nvim_lsp = require("cmp_nvim_lsp")
+capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
+
 require("lspconfig").yamlls.setup({
-	capabilities = capabilities;
+  capabilities = capabilities,
   settings = {
     yaml = {
-      format = {
-        enable = true,  -- Enables formatting
-      },
-			validate = true;
-			hover = true;
-			completion =true;
+      format = { enable = true },     -- Enables formatting
+      validate = true,                -- Enables schema validation
+      hover = true,                   -- Enables hover information
+      completion = true,              -- Enables autocompletion
       schemas = {
         kubernetes = "*.yaml",
+        ["https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/crds/application-crd.yaml"] = "argocd-app.yaml",
+        ["https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/crds/appproject-crd.yaml"] = "argocd-project.yaml",
         ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
+        ["http://json.schemastore.org/github-action"] = ".github/action.yml",
+      },
+      schemaStore = {
+        enable = true,  -- Automatically pull in schemas from SchemaStore
       },
     },
   },
@@ -25,8 +32,5 @@ vim.api.nvim_create_autocmd("BufWritePre", {
   end,
 })
 
--- Enable completion support from nvim-cmp
-local cmp_nvim_lsp = require("cmp_nvim_lsp")
-capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
 
 
