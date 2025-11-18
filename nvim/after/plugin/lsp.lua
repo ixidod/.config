43b31f17a1f83d277b900e7cf4be0a98ac7f1f7e
Patrycjusz Myszka <pat.myszka@gmail.com>
2025-11-18 11:11:46 +0000

    tide up

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -3,20 +3,26 @@ vim.opt.signcolumn = 'yes'
 
 -- --- LSP-driven keymaps (only when a client attaches)
 vim.api.nvim_create_autocmd('LspAttach', {
-	desc = 'LSP actions',
-	callback = function(event)
-		local opts = { buffer = event.buf }
-		vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-		vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-		vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-		vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-		vim.keymap.set('n', 'go', vim.lsp.buf.type_definition, opts)
-		vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-		vim.keymap.set('n', 'gs', vim.lsp.buf.signature_help, opts)
-		vim.keymap.set('n', '<F2>', vim.lsp.buf.rename, opts)
-		vim.keymap.set({ 'n', 'x' }, '<F3>', function() vim.lsp.buf.format({ async = true }) end, opts)
-		vim.keymap.set('n', '<F4>', vim.lsp.buf.code_action, opts)
-	end,
+  desc = 'LSP actions',
+  callback = function(event)
+    local opts = { buffer = event.buf }
+    vim.keymap.set('n', '<leader>e', function()
+      vim.diagnostic.open_float(nil, { focus = false, border = 'rounded' })
+    end, opts)
+
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', 'go', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', 'gs', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set('n', '<F2>', vim.lsp.buf.rename, opts)
+    vim.keymap.set({ 'n', 'x' }, '<F3>', function()
+      vim.lsp.buf.format { async = true }
+    end, opts)
+    vim.keymap.set('n', '<F4>', vim.lsp.buf.code_action, opts)
+  end,
 })
 
 -- Capabilities from nvim-cmp
@@ -24,92 +30,126 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 -- Helper: merge with existing defaults (provided by nvim-lspconfig)
 local function with_defaults(name, overrides)
-	local base = vim.deepcopy(vim.lsp.config[name] or {})
-	return vim.tbl_deep_extend('force', base, overrides or {})
+  local base = vim.deepcopy(vim.lsp.config[name] or {})
+  return vim.tbl_deep_extend('force', base, overrides or {})
 end
 
 -- ---- Per-server configs (new style)
 
 vim.lsp.config['yamlls'] = with_defaults('yamlls', {
-	capabilities = capabilities,
-	settings = {
-		yaml = {
-			schemaStore = { enable = true },
-			schemas = {
-				['https://raw.githubusercontent.com/awslabs/goformation/master/schema/cloudformation.schema.json'] = 'cf*.yml',
-				['https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json'] =
-				'.gitlab-ci.yml',
-				['https://json.schemastore.org/github-workflow'] = '.github/workflows/*.yml',
-				kubernetes = 'k8s/*.yaml',
-			},
-		},
-		validate = true,
-		completion = true,
-		hover = true,
-	},
+  capabilities = capabilities,
+  settings = {
+    yaml = {
+      schemaStore = { enable = true },
+      schemas = {
+        ['https://raw.githubusercontent.com/awslabs/goformation/master/schema/cloudformation.schema.json'] = 'cf*.yml',
+        ['https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json'] = '.gitlab-ci.yml',
+        ['https://json.schemastore.org/github-workflow'] = '.github/workflows/*.yml',
+        kubernetes = 'k8s/*.yaml',
+      },
+    },
+    validate = true,
+    completion = true,
+    hover = true,
+  },
+})
+
+vim.lsp.config['pyright'] = with_defaults('pyright', {
+  capabilities = capabilities,
+  settings = {
+    python = {
+      venvPath = '.',
+      venv = 'venv',
+      analysis = {
+        autoSearchPaths = true,
+        useLibraryCodeForTypes = true,
+        diagnosticMode = 'workspace',
+        typeCheckingMode = 'basic',
+        autoImportCompletions = true,
+
+        reportUnusedImport = 'warning',
+        reportUnusedVariable = 'warning',
+        reportMissingImports = 'warning',
+        reportOptionalMemberAccess = 'warning',
+        reportOptionalSubscript = 'warning',
+      },
+    },
+  },
 })
 
 vim.lsp.config['gopls'] = with_defaults('gopls', {
-	capabilities = capabilities,
+  capabilities = capabilities,
 })
 
 vim.lsp.config['lua_ls'] = with_defaults('lua_ls', {
-	capabilities = capabilities,
-	settings = {
-		Lua = {
-			runtime = { version = 'LuaJIT' },
-			diagnostics = { globals = { 'vim' } },
-			workspace = { library = vim.api.nvim_get_runtime_file('', true) },
-			telemetry = { enable = false },
-		},
-	},
+  capabilities = capabilities,
+  settings = {
+    Lua = {
+      runtime = { version = 'LuaJIT' },
+      diagnostics = { globals = { 'vim' } },
+      workspace = { library = vim.api.nvim_get_runtime_file('', true) },
+      telemetry = { enable = false },
+    },
+  },
 })
 
 vim.lsp.config['terraformls'] = with_defaults('terraformls', {
-	capabilities = capabilities,
-	filetypes = { 'terraform', 'tf', 'hcl' },
+  capabilities = capabilities,
+  filetypes = { 'terraform', 'tf', 'hcl' },
+  settings = {
+    terraform = {
+      languageServer = {
+        experimentalFeatures = {
+          ignoreSingleFileWarning = true,
+        },
+      },
+    },
+  },
 })
 
 vim.lsp.config['helm_ls'] = with_defaults('helm_ls', {
-	capabilities = capabilities,
+  capabilities = capabilities,
 })
 
 vim.lsp.config['html'] = with_defaults('html', {
-	capabilities = capabilities,
-	filetypes = { 'html' },
-	settings = {
-		html = {
-			format = { enable = true },
-			hover = { documentation = true },
-			validate = true,
-			autoClosingTags = true,
-		},
-	},
+  capabilities = capabilities,
+  filetypes = { 'html' },
+  settings = {
+    html = {
+      format = { enable = true },
+      hover = { documentation = true },
+      validate = true,
+      autoClosingTags = true,
+    },
+  },
 })
 
 -- ---- Start the right server when a buffer with a matching filetype opens
 
 local ft_to_server = {
-	yaml = 'yamlls',
-	yml = 'yamlls',
-	go = 'gopls',
-	lua = 'lua_ls',
-	terraform = 'terraformls',
-	tf = 'terraformls',
-	hcl = 'terraformls',
-	helm = 'helm_ls',
-	html = 'html',
+  yaml = 'yamlls',
+  yml = 'yamlls',
+  go = 'gopls',
+  lua = 'lua_ls',
+  terraform = 'terraformls',
+  tf = 'terraformls',
+  hcl = 'terraformls',
+  helm = 'helm_ls',
+  html = 'html',
+  python = 'pyright',
 }
 
 vim.api.nvim_create_autocmd('FileType', {
-	pattern = vim.tbl_keys(ft_to_server),
-	callback = function(args)
-		local ft = vim.bo[args.buf].filetype
-		local name = ft_to_server[ft]
-		if not name then return end
-		local cfg = vim.deepcopy(vim.lsp.config[name] or {})
-		-- Make sure the client associates with the current buffer
-		cfg.bufnr = args.buf
-		vim.lsp.start(cfg)
-	end,
+  pattern = vim.tbl_keys(ft_to_server),
+  callback = function(args)
+    local ft = vim.bo[args.buf].filetype
+    local name = ft_to_server[ft]
+    if not name then
+      return
+    end
+    local cfg = vim.deepcopy(vim.lsp.config[name] or {})
+    -- Make sure the client associates with the current buffer
+    cfg.bufnr = args.buf
+    vim.lsp.start(cfg)
+  end,
 })
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -1,12 +1,9 @@
-
 export EDITOR=nvim
 bindkey -v
 
 alias nvim-update='for d in /Users/iota/.local/share/nvim/site/pack/plugins/start/*/.git; do (cd "${d%/.git}" && echo "Updating $(basename "$PWD")" && git pull --ff-only); done'
 
 alias vi='nvim'
-alias k="kubectl"
-alias kgp="kubectl get pods"
 setopt NO_CASE_GLOB
 source <(fzf --zsh)
 
@@ -14,74 +11,11 @@ export PATH="/opt/homebrew/bin:$PATH"
 export GOPATH=/Users/iota/.go
 export PATH=$GOPATH/bin:$PATH
 
-export DOCKER_DEFAULT_PLATFORM=linux/amd64
-
-#source "$(brew --prefix)/share/google-cloud-sdk/completion.zsh.inc"
-
 fpath=(/opt/homebrew/share/zsh/site-functions \\$fpath)
-autoload -U compinit; compinit
 autoload -U +X bashcompinit && bashcompinit
 
 eval "$(/opt/homebrew/bin/brew shellenv)"
-source <(fzf --zsh)
 complete -o nospace -C /opt/homebrew/bin/terraform terraform
 
-# The following lines have been added by Docker Desktop to enable Docker CLI completions.
-fpath=(/Users/iota/.docker/completions $fpath)
 autoload -Uz compinit
 compinit
-
-# Minimal status: name, branch, ahead/behind, dirty; does NOT change your cwd
-nvim-status() {
-  ( setopt localoptions null_glob
-    local roots=("$HOME/.local/share/nvim/site/pack"/*/start)
-    for root in "${roots[@]}"; do
-      for g in "$root"/*/.git(N); do
-        (
-          cd "${g%/.git}" || exit
-          local plugin branch upstream default_branch behind ahead dirty
-          plugin=${PWD##*/}
-          branch=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
-          upstream=$(git rev-parse --abbrev-ref --symbolic-full-name @{u} 2>/dev/null)
-          if [[ -z "$upstream" ]]; then
-            default_branch=$(git remote show origin 2>/dev/null | awk '/HEAD branch/ {print $NF}')
-            [[ -z "$default_branch" ]] && default_branch=master
-            upstream="origin/$default_branch"
-          fi
-          git fetch --quiet origin 2>/dev/null
-          behind=$(git rev-list --count HEAD.."$upstream" 2>/dev/null)
-          ahead=$(git rev-list --count "$upstream"..HEAD 2>/dev/null)
-          git diff --quiet --ignore-submodules -- 2>/dev/null || dirty="*"
-          printf "%-28s %-12s a:%-3s b:%-3s %s\n" \
-            "$plugin" "${branch:-?}" "$ahead" "$behind" "$dirty"
-        )
-      done
-    done
-  )
-}
-
-# Only show plugins that are behind; also no cwd change
-nvim-outdated() {
-  ( setopt localoptions null_glob
-    local roots=("$HOME/.local/share/nvim/site/pack"/*/start)
-    for root in "${roots[@]}"; do
-      for g in "$root"/*/.git(N); do
-        (
-          cd "${g%/.git}" || exit
-          local plugin upstream default_branch behind
-          plugin=${PWD##*/}
-          upstream=$(git rev-parse --abbrev-ref --symbolic-full-name @{u} 2>/dev/null)
-          if [[ -z "$upstream" ]]; then
-            default_branch=$(git remote show origin 2>/dev/null | awk '/HEAD branch/ {print $NF}')
-            [[ -z "$default_branch" ]] && default_branch=master
-            upstream="origin/$default_branch"
-          fi
-          git fetch --quiet origin 2>/dev/null
-          behind=$(git rev-list --count HEAD.."$upstream" 2>/dev/null)
-          [[ "$behind" -gt 0 ]] && echo "$plugin is behind by $behind commit(s)"
-        )
-      done
-    done
-  )
-}
-
