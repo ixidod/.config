de388e472f9e1d8434985791bb62fdad6ab3baaa
Patrycjusz Myszka <pat.myszka@gmail.com>
2025-11-25 18:26:56 +0000

    bare minimum

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,12 @@
 vim.cmd('syntax on')
 
+vim.opt.hlsearch = false
+vim.opt.undofile = true
+vim.opt.undodir = os.getenv('HOME') .. '/.vim/undodir'
+vim.opt.scrolloff = 8
+vim.opt.ignorecase = true
+vim.opt.autoindent = true
+vim.opt.swapfile = false
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.tabstop = 2
@@ -8,14 +15,8 @@ vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.completeopt = { 'menuone', 'noselect' }
 vim.opt.ignorecase = true
+vim.cmd.filetype("plugin indent on")
 
----@class UserVimGlobals: vim.var_accessor
----@field mapleader string
----@field netrw_banner integer
----@field netrw_liststyle integer
----@field netrw_browse_split integer
----@field netrw_winsize integer
----@cast vim.g UserVimGlobals
 
 vim.g.mapleader = ' '
 
@@ -24,24 +25,3 @@ vim.g.netrw_liststyle = 3
 vim.g.netrw_browse_split = 0
 vim.g.netrw_winsize = 25
 
-vim.pack.add {
-  { src = 'https://github.com/nvim-lua/plenary.nvim' },
-  { src = 'https://github.com/fatih/vim-go' },
-  { src = 'https://github.com/rose-pine/neovim' },
-  { src = 'https://github.com/nanotee/sqls.nvim' },
-  { src = 'https://github.com/nvim-treesitter/nvim-treesitter' },
-  { src = 'https://github.com/tpope/vim-dadbod' },
-  { src = 'https://github.com/kristijanhusak/vim-dadbod-ui' },
-  { src = 'https://github.com/kristijanhusak/vim-dadbod-completion' },
-  { src = 'https://github.com/windwp/nvim-autopairs' },
-  { src = 'https://github.com/nvim-telescope/telescope.nvim' },
-  { src = 'https://github.com/nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-}
-require('config.theme').setup()
-require('config.autopairs').setup()
-require('config.telescope').setup()
-require('config.treesitter').setup()
-require('config.completion').setup()
-require('config.netrw').setup()
-require('config.dadbod').setup()
-require('config.languages').setup()
--- nvim/lua/config/netrw.lua
+++
@@ -1,7 +0,0 @@
-local M = {}
-
-function M.setup()
-  vim.keymap.set('n', '<leader>pv', vim.cmd.Ex, { desc = 'Back to netrw listing' })
-end
-
-return M
