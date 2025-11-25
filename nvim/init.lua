d0cabaa780ac7b42ea1c52a3145b071cf46fe144
Patrycjusz Myszka <pat.myszka@gmail.com>
2025-11-25 20:57:30 +0000

    ss

--- nvim/init.lua
+++ nvim/init.lua
@@ -152,7 +152,7 @@ vim.lsp.config('lua_ls', {
   settings = {
     Lua = {
       runtime = { version = 'LuaJIT' },
-      diagnostics = { globals = { 'vim' } },
+      diagnostics = { globals = { 'vim', 'vim.g' } },
       workspace = {
         checkThirdParty = false,
         library = vim.api.nvim_get_runtime_file('', true),
