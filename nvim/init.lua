17ea581b29085754eed1d7653576b3954f71cc23
Patrycjusz Myszka <pat.myszka@gmail.com>
2025-11-26 22:05:24 +0000

    vi

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,62 +1,58 @@
-vim.cmd('syntax on')
-vim.cmd('filetype plugin indent on')
-vim.cmd('colorscheme ixidod_min')
+vim.cmd("syntax on")
+vim.cmd("filetype plugin indent on")
+vim.cmd("colorscheme ixidod_min")
 
 vim.api.nvim_set_hl(0, "Directory", { link = "Normal" })
 
-local opt            = vim.opt
-local g              = vim.g
+local opt = vim.opt
+local g = vim.g
 
 opt.modelines = 0
 
-opt.hlsearch         = false
-opt.incsearch        = true
-opt.ignorecase       = true
-opt.smartcase        = true
-opt.undofile         = true
-opt.undodir          = vim.fn.stdpath('data') .. '/undo'
+opt.hlsearch = false
+opt.incsearch = true
+opt.ignorecase = true
+opt.smartcase = true
+opt.undofile = true
+opt.undodir = vim.fn.stdpath("data") .. "/undo"
 
-opt.swapfile         = false
-opt.backup           = false
-opt.writebackup      = false
+opt.swapfile = false
+opt.backup = false
+opt.writebackup = false
 
 opt.numberwidth = 3
-opt.number           = true
-opt.relativenumber   = true
-opt.signcolumn       = 'yes'
+opt.number = true
+opt.relativenumber = true
+opt.signcolumn = "yes"
 
-opt.scrolloff        = 8
-opt.wrap             = false
+opt.scrolloff = 8
+opt.wrap = false
 
-opt.tabstop          = 2
-opt.softtabstop      = 2
-opt.shiftwidth       = 2
-opt.expandtab        = true
-opt.smartindent      = true
+opt.tabstop = 2
+opt.softtabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.smartindent = true
 
-opt.completeopt      = { 'menuone', 'noselect' }
-opt.updatetime       = 250
-opt.termguicolors    = true
+g.mapleader = " "
 
-g.mapleader          = ' '
+g.loaded_netrw = 1
+g.loaded_netrwPlugin = 1
+g.loaded_netrwSettings = 1
 
-g.netrw_banner       = 0
-g.netrw_liststyle    = 3
-g.netrw_browse_split = 0
-g.netrw_winsize      = 25
+local map = vim.keymap.set
+local kopt = { noremap = true, silent = true }
 
-local map            = vim.keymap.set
-local kopt           = { noremap = true, silent = true }
+map("n", "<leader>bn", ":bnext<CR>", kopt)
+map("n", "<leader>bp", ":bprevious<CR>", kopt)
+map("n", "<leader>bd", ":bd<CR>", kopt)
+map("n", "<leader>bb", ":b#<CR>", kopt)
+map("n", "<leader>bl", ":ls<CR>", kopt)
 
-map('n', '<leader>bn', ':bnext<CR>', kopt)
-map('n', '<leader>bp', ':bprevious<CR>', kopt)
+map("n", "<leader>w", ":write<CR>", kopt)
+map("n", "<leader>q", ":quit<CR>", kopt)
 
-map('n', '<leader>w', ':write<CR>', kopt)
-map('n', '<leader>q', ':quit<CR>', kopt)
-map('n', '<leader>h', ':nohlsearch<CR>', kopt)
-map('n', '<leader>e', ':Ex<CR>', kopt)
-
-map('n', '<C-h>', '<C-w>h', kopt)
-map('n', '<C-j>', '<C-w>j', kopt)
-map('n', '<C-k>', '<C-w>k', kopt)
-map('n', '<C-l>', '<C-w>l', kopt)
+map("n", "<C-h>", "<C-w>h", kopt)
+map("n", "<C-j>", "<C-w>j", kopt)
+map("n", "<C-k>", "<C-w>k", kopt)
+map("n", "<C-l>", "<C-w>l", kopt)
